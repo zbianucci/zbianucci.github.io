8da62a5c2d7ebc365c13b46003b66116f146c972
zbianucci <65574431+zbianucci@users.noreply.github.com>
2021-05-06 00:02:48 -0400

    Add files via upload

--- towerdefense/script.js
+++ towerdefense/script.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
 canvas.width = 900;
 canvas.height = 600;
 
@@ -7,6 +7,9 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+//let defenderCost = 100;
+let numberOfResources = 300;
 
 //mouse
 const mouse = {
@@ -14,22 +17,22 @@ const mouse = {
   y: undefined,
   width: 0.1,
   height: 0.1,
-}
+};
 let canvasPosition = canvas.getBoundingClientRect();
-canvas.addEventListener('mousemove', function (e) {
+canvas.addEventListener("mousemove", function (e) {
   mouse.x = e.x - canvasPosition.left;
   mouse.y = e.y - canvasPosition.top;
 });
-canvas.addEventListener('mouseleave', function () {
+canvas.addEventListener("mouseleave", function () {
   mouse.x = undefined;
   mouse.y = undefined;
-})
+});
 
 // game board
 const controlsBar = {
   width: canvas.width,
   height: cellSize,
-}
+};
 
 class Cell {
   constructor(x, y) {
@@ -40,7 +43,7 @@ class Cell {
   }
   draw() {
     if (mouse.x && mouse.y && collision(this, mouse)) {
-      ctx.strokeStyle = 'black';
+      ctx.strokeStyle = "black";
       ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
   }
@@ -61,15 +64,61 @@ function handleGameGrid() {
 
 //projectiles
 //defenders
+class Defender {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+    this.shooting = false; //enemies will only shoot when defender is on their row
+    this.health = 100;
+    this.projectiles = [];
+    this.timer = 0;
+  }
+  draw() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px Arial';
+    ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
+  }
+}
+canvas.addEventListener('click', function () {
+  const gridPositionX = mouse.x - (mouse.x % cellSize);
+  const gridPositionY = mouse.y - (mouse.y % cellSize);
+  if (gridPositionY < cellSize) return; //mouse in blue bar
+  for (let i = 0; i < defenders.length; i++){
+    if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY)
+    return;
+  }
+  let defenderCost = 100;
+  if (numberOfResources >= defenderCost) {
+    defenders.push(new Defender(gridPositionX, gridPositionY));
+    numberOfResources -= defenderCost;
+  }
+});
+function handleDefenders() {
+  for (let i = 0; i < defenders.length; i++) {
+    defenders[i].draw();
+  }
+}
 
 //enemies
 //resources
 //utilities
+function handleGameStatus(){
+  ctx.fillStyle = 'gold';
+  ctx.font = '30px Arial';
+  ctx.fillText('Resources: ' + numberOfResources, 20, 55);
+}
+
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'blue';
+  ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
   handleGameGrid();
+  handleDefenders();
+  handleGameStatus();
   requestAnimationFrame(animate);
 }
 animate();
@@ -84,5 +133,5 @@ function collision(first, second) {
     )
   ) {
     return true;
-  };
-};
+  }
+}
